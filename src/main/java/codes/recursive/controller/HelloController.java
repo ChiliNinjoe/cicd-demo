7ec941acaca4d34057d6fa8c456dc5731edec0b2
Joe Ngo <chilijoe@gmail.com>
2020-11-29 21:00:06 +0800

    added version endpoint

--- src/main/java/codes/recursive/controller/HelloController.java
+++ src/main/java/codes/recursive/controller/HelloController.java
@@ -5,8 +5,13 @@ import io.micronaut.http.annotation.*;
 @Controller("/hello")
 public class HelloController {
 
-    @Get(uri="/", produces="text/plain")
-    public String index() {
-        return "Example Response";
-    }
+  @Get(uri = "/", produces = "text/plain")
+  public String index() {
+    return "Example Response";
+  }
+
+  @Get("/version")
+  public HttpResponse<Map> getVersion() {
+    return HttpResponse.ok(Map.of("version", "0.1"));
+  }
 }
